7407f37d95a4ab2e3e8c507c9b1fb2d83f7f8204
Rylan Evans <rylanevans@hotmail.com>
2017-10-08 00:40:58 -0600

    Added Mask and Shadow

---
+++ Helpers/UIImageViewWithMask.swift
@@ -0,0 +1,69 @@
+//
+//  UIImageViewWithMask.swift
+//  FHE
+//
+//  Created by Rylan Evans on 10/7/17.
+//  Copyright © 2017 Rylan Evans. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIImageViewWithMask: UIImageView {
+    
+    var imageToMaskView = UIImageView()
+    var maskingImageView = UIImageView()
+    
+    @IBInspectable
+    var shadowOpacity: Float = 0 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable
+    var imageToMask: UIImage? {
+        didSet {
+            imageToMaskView.image = imageToMask
+            updateView()
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize = CGSize(width: 0, height: 0) {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    func updateView() {
+        
+        if imageToMaskView.image != nil {
+            imageToMaskView.frame = bounds
+            imageToMaskView.contentMode = .scaleAspectFill
+            
+            
+            maskingImageView.image = image // The Mask
+            maskingImageView.frame = bounds
+            maskingImageView.contentMode = .scaleToFill
+            
+            imageToMaskView.layer.mask = maskingImageView.layer
+            
+            addSubview(imageToMaskView)
+        }
+    }
+}
