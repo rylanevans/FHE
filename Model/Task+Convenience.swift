4de8864cd12e680ce15ffb115fd195353e694356
Rylan Evans <rylanevans@hotmail.com>
2017-10-17 10:55:04 -0600

    Making more changes to Core Data

---
+++ Model/Task+Convenience.swift
@@ -0,0 +1,24 @@
+//
+//  Task+Convenence.swift
+//  FHE
+//
+//  Created by Rylan Evans on 10/17/17.
+//  Copyright © 2017 Rylan Evans. All rights reserved.
+//
+//
+
+import CoreData
+import UIKit
+
+extension Task {
+    
+    convenience init(enabled: Bool, name: String, order: Int64, assigned: Bool, in context:NSManagedObjectContext) {
+        
+        self.init(context:context)
+        
+        self.enabled = enabled
+        self.assigned = assigned
+        self.name = name
+        self.order = order
+    }
+}
