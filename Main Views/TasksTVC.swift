cea7ea23accc9522310e87ffa88b11107eed155f
Rylan <rylanjevans@gmail.com>
2017-11-04 11:26:08 -0600

    Just removed print test

--- Main Views/TasksTVC.swift
+++ Main Views/TasksTVC.swift
@@ -19,8 +19,6 @@ class TasksTVC: UITableViewController {
             NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0, green: 0.4755483866, blue: 0.9911283851, alpha: 1),
             NSAttributedStringKey.font: UIFont(name: "Noteworthy-Bold", size: 35)!
         ]
-        print("Test")
-        print("2")
         
         clickSoundURL()
         
